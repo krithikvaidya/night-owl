2ea5127c97bc00521ef7de6a674e2b41610804f1
krithikvaidya <krithikvaidya@gmail.com>
2020-06-11 18:10:34 +0530

    Add requirements.txt

--- night_owl/models.py
+++ night_owl/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class NC1Products(models.Model):
+class NCProducts(models.Model):
 
     #item id of each item in the menu will be its primary key
     name = models.CharField(max_length=200)
@@ -8,7 +8,7 @@ class NC1Products(models.Model):
     veg=models.BooleanField(default=True)
     currently_present=models.BooleanField()
     MY_CHOICES = ((1,'Hot and Cold Beverages'),(2,'Dosa Items'), (3, 'Sandwiches Bar'))
-    filter = models.IntegerField(choices=MY_CHOICES)
+    filters = models.IntegerField(choices=MY_CHOICES)
 
     class Meta(object):
         verbose_name_plural = 'Menu items of Block 3 Night Canteen'
@@ -16,7 +16,7 @@ class NC1Products(models.Model):
     def __str__(self):
         return self.name
 
-class PaidOrdersNC1(models.Model):  # each object of this model represents a successfully created order in NC1
+class PaidOrders(models.Model):  # each object of this model represents a successfully created order in NC1
     ph_no = models.BigIntegerField()
     gpay_ph_no = models.BigIntegerField()
     block = models.CharField(max_length=15)
@@ -29,72 +29,6 @@ class PaidOrdersNC1(models.Model):  # each object of this model represents a suc
 
     class Meta(object):
         verbose_name_plural = 'List of orders created - Block 3 Night Canteen'
-
-    def __str__(self):
-        return 'Order number - ' + str(self.pk)
-
-class NC2Products(models.Model):
-
-    #item id of each item in the menu will be its primary key
-    name = models.CharField(max_length=200)
-    price = models.IntegerField()
-    veg=models.BooleanField(default=True)
-    currently_present=models.BooleanField()
-    MY_CHOICES = ((1,'Hot and Cold Beverages'),(2,'Dosa Items'), (3, 'Sandwiches Bar'))
-    filter = models.IntegerField(choices=MY_CHOICES)
-    
-
-    class Meta(object):
-        verbose_name_plural = 'Menu items of Block 7 Night Canteen'
-
-    def __str__(self):
-        return self.name
-
-class PaidOrdersNC2(models.Model):  # each object of this model represents a successfully created order in NC2
-    ph_no = models.BigIntegerField()
-    gpay_ph_no = models.BigIntegerField()
-    block = models.CharField(max_length=15)
-    order_comments = models.TextField(null=True, blank=True)
-    item_name = models.TextField()
-    price = models.TextField()
-    quantity = models.TextField()
-    MY_CHOICES = (('a','Pending'),('b','Delivered'),('c','Rejected'))
-    filters = models.CharField(choices=MY_CHOICES, default='a', max_length=10)
-
-    class Meta(object):
-        verbose_name_plural = 'List of orders created - Block 7 Night Canteen'
-
-    def __str__(self):
-        return 'Order number - ' + str(self.pk)
-
-class NC3Products(models.Model):
-    #item id of each item in the menu will be its primary key
-    name = models.CharField(max_length=200)
-    price = models.IntegerField()
-    veg=models.BooleanField(default=True)
-    currently_present=models.BooleanField()
-    MY_CHOICES = ((1,'Noodle Items'),(2,'Rice Items'))
-    filter = models.IntegerField(choices=MY_CHOICES)
-
-    class Meta(object):
-        verbose_name_plural = 'Menu items of Girl\'s Block Night Canteen'
-
-    def __str__(self):
-        return self.name
-
-class PaidOrdersNC3(models.Model):  # each object of this model represents a successfully created order in NC3
-    ph_no = models.BigIntegerField()
-    gpay_ph_no = models.BigIntegerField()
-    block = models.CharField(max_length=15)
-    order_comments = models.TextField(null=True, blank=True)
-    item_name = models.TextField()
-    price = models.TextField()
-    quantity = models.TextField()
-    MY_CHOICES = (('a','Pending'),('b','Delivered'),('c','Rejected'))
-    filters = models.CharField(choices=MY_CHOICES, default='a', max_length=10)
-
-    class Meta(object):
-        verbose_name_plural = 'List of orders created - Girl\'s Block Night Canteen'
 
     def __str__(self):
         return 'Order number - ' + str(self.pk)
